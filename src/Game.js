4f78787d015e9e72f8053156c8302509e304bdc4
CeilingWin <56648892+CeilingWin@users.noreply.github.com>
2020-11-16 11:23:48 +0700

    v2

--- src/Game.js
+++ src/Game.js
@@ -14,6 +14,25 @@ var Game = cc.Scene.extend({
         colorLayer.ignoreAnchorPointForPosition(false)
         colorLayer.x = cc.winSize.width/2;
         colorLayer.y = cc.winSize.height/2;
+        var snake = new Snake();
+        snake.setPosition(cc.p(100,100));
         this.addChild(colorLayer);
+        this.addChild(snake);
+    }
+})
+
+var Snake = cc.Layer.extend({
+    ctor:function(){
+        this._super(300,200);
+        this.init();
+        return true;
+    },
+    init:function(){
+        var head = cc.Sprite("res/snake/head_right.png");
+        var body = cc.Sprite("res/snake/body_topleft.png");
+        head.setPosition(cc.p(50,50));
+        body.setPosition(cc.p(50,300));
+        this.addChild(head);
+        this.addChild(body);
     }
 })
